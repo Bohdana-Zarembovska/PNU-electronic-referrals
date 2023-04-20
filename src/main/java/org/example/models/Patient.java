6cfb00effa147fd0aa504480120073cccfcea859
Bohdana-Zarembovska <zarembovskabohdana@gmail.com>
2023-04-20 17:56:01 +0300

    ...
    ...

--- src/main/java/org/example/models/Patient.java
+++ src/main/java/org/example/models/Patient.java
@@ -1,5 +1,14 @@
 package org.example.models;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import java.util.Date;
 
 @Entity
@@ -85,5 +94,5 @@ enum BloodType {
 }
 
 enum Sex {
-    MALE, FEMALE, OTHER;
+    MALE, FEMALE;
 }
