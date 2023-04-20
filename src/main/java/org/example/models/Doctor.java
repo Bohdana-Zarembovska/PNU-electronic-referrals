af7d8041980fc7a2475a25c9aff3f139a2106e0d
Bohdana-Zarembovska <zarembovskabohdana@gmail.com>
2023-04-20 23:08:51 +0300

    edded

--- src/main/java/org/example/models/Doctor.java
+++ src/main/java/org/example/models/Doctor.java
@@ -1,5 +1,10 @@
 package org.example.models;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "doctor")
